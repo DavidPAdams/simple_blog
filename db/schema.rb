989654ae0fde73ee513b7ffc0edb3f467f6f8ffd
David Adams <david61795@gmail.com>
2018-07-05 20:17:17 -0700

    articles & comments view files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180706024706) do
+ActiveRecord::Schema.define(version: 20180706030611) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20180706024706) do
     t.text "comment_entry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "article_id"
   end
 
 end
